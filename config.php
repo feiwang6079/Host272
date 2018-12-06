b7c209b46a599f9038f20e4bd11ab5d53d47840d
fei wang <fei.wang@sjsu.edu>
2018-12-06 09:49:38 -0800

    first commit

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+// 创建连接
+$conn = mysqli_connect("db-30bemv1qn.aliwebs.com", "30bemv1qn", "123456", "30bemv1qn");
+
+// 检测连接
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
